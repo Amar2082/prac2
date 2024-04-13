eb92ed48d0d76689b56f027cd2fd063581aee5de
Amar2082 <166601398+Amar2082@users.noreply.github.com>
2024-04-13 18:09:45 +0530

    Add files via upload

---
+++ assignment4_4.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+class Employee
+{
+    private:
+    int id;
+    int salary;
+    string name;
+    
+    public:
+    Employee(int id, int salary,string name)
+    {
+       this->id=id;
+       this->salary=salary;
+       this->name=name;
+      
+    }
+    
+    void show()
+    {
+        cout<<"id:"<<id<<"salary:"<<salary<<"name:"<<name<<endl;
+    }
+
+
+};
+
+class Manager:public Employee
+{
+    public:
+    int incentive;
+    int DeptId;
+
+    Manager(int id, int salary,string name,int incentive,int DeptId):Employee(id,salary,name)
+    {
+        this->incentive=incentive;
+        this->DeptId=DeptId;
+
+    }
+    
+    void calculate_salary()
+    {
+      int salary=0;
+      salary=salary+incentive;
+
+      cout<<" manager salary:"<<salary<<endl;
+    }
+
+    
+
+};
+
+
+int main()
+{
+
+    Manager mobj(4,300000,"ABC",20000,10);
+    mobj.show();
+    mobj.calculate_salary();
+
+  return 0;
+}
